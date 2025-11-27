981ab54c8da10a60d44e35f42319b7b0ef5f1e53
sdhaval97 <sdhaval97@gmail.com>
2025-11-27 19:11:29 +0530

    installed tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'marigold': '#FCB800',
+        'seal-brown': '#472109',
+      },
+      fontFamily: {
+        'made-tommy': ['"Made Tommy"', 'sans-serif'],
+        'christmas-sheep': ['"Christmas Sheep"', 'cursive'],
+      },
+    },
   },
   plugins: [],
 }
