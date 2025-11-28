120bf419b28eafa2db727282dee93f18db10a3fe
sdhaval97 <sdhaval97@gmail.com>
2025-11-28 11:58:55 +0530

    updated the header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,38 +1,36 @@
-import { Link } from 'react-router-dom';
-
 const Header = () => {
   return (
     <header className="bg-seal-brown text-white py-4 sticky top-0 z-50">
       <div className="container mx-auto px-4 flex items-center justify-between">
         {/* Logo */}
-        <Link to="/" className="flex items-center">
+        <div className="flex items-center">
           <img
             src="/logo-white.png"
             alt="KHiCHOS"
             className="h-12 md:h-16"
           />
-        </Link>
+        </div>
 
         {/* Navigation */}
         <nav className="flex gap-6 md:gap-8">
-          <Link
-            to="/"
+          <a
+            href="#"
             className="font-made-tommy font-medium hover:text-marigold transition-colors"
           >
             Home
-          </Link>
-          <Link
-            to="/about"
+          </a>
+          {/* <a
+            href="#"
             className="font-made-tommy font-medium hover:text-marigold transition-colors"
           >
             Our Story
-          </Link>
-          <Link
-            to="/contact"
+          </a>
+          <a
+            href="#"
             className="font-made-tommy font-medium hover:text-marigold transition-colors"
           >
             Contact
-          </Link>
+          </a> */}
         </nav>
       </div>
     </header>
